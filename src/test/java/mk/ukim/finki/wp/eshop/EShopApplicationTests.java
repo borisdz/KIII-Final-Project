28492f19db1cfc9024a56e0d155135b61d450469
Boris Djotov <borisdzotov@gmail.com>
2024-06-15 20:18:58 +0200

    Modified Dockerfile and project structure and files

--- src/test/java/mk/ukim/finki/wp/eshop/EShopApplicationTests.java
+++ src/test/java/mk/ukim/finki/wp/eshop/EShopApplicationTests.java
@@ -3,11 +3,6 @@ package mk.ukim.finki.wp.eshop;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class EShopApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//@SpringBootTest
+//class EShopApplicationTests {
+//}
