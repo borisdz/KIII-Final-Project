fcc241fe960988872f0cc5159435c6359ddeffc3
Boris Djotov <borisdzotov@gmail.com>
2024-06-26 20:07:18 +0200

    App is unable to connect to db

--- kubernetes/run-kube.bat
+++ kubernetes/run-kube.bat
@@ -8,7 +8,9 @@ kubectl delete ingress --all -n kiii-project-borisdz-185022
 echo Running all manifests.
 kubectl apply -f namespace.yml
 kubectl apply -f secret-postgresql.yml
+kubectl apply -f secret-app.yml
 kubectl apply -f configmap-app.yml
+kubectl apply -f configmap-postgresql.yml
 kubectl apply -f postgres-pv.yml
 kubectl apply -f postgres-pvc.yml
 kubectl apply -f deployment-postgres.yml
@@ -16,4 +18,3 @@ kubectl apply -f service-postgresql.yml
 kubectl apply -f deployment-app.yml
 kubectl apply -f service-app.yml
 kubectl apply -f ingress.yml
-pause
