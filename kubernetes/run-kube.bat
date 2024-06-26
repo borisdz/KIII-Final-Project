d15186770492ec420f775441cd0a14c457b5d56e
Boris Djotov <borisdzotov@gmail.com>
2024-06-26 17:16:36 +0200

    Made changes on PC

--- kubernetes/run-kube.bat
+++ kubernetes/run-kube.bat
@@ -1,11 +1,11 @@
 @echo off
-echo deleting all current running resources in kubernetes.
+echo Deleting all current running resources in kubernetes.
 kubectl delete all --all -n kiii-project-borisdz-185022
 kubectl delete configmap --all -n kiii-project-borisdz-185022
 kubectl delete secret --all -n kiii-project-borisdz-185022
 kubectl delete pvc --all -n kiii-project-borisdz-185022
 kubectl delete ingress --all -n kiii-project-borisdz-185022
-echo running all manifests.
+echo Running all manifests.
 kubectl apply -f namespace.yml
 kubectl apply -f secret-postgresql.yml
 kubectl apply -f configmap-app.yml
